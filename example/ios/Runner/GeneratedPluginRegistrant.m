42be2dfebcc456f505ac131874b96ba487fd80dd
Samuel Adekunle <samuelbeebest@gmail.com>
2021-11-12 13:47:36 +0100

    updates

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<slaba/SlabaPlugin.h>)
+#import <slaba/SlabaPlugin.h>
+#else
+@import slaba;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SlabaPlugin registerWithRegistrar:[registry registrarForPlugin:@"SlabaPlugin"]];
+}
+
+@end
